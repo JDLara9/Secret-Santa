a407ef3235a9daf6a8d9f42c7a251208b2124b93
“JDLara9” <Joshualara9@gmail.com>
2024-10-25 16:55:22 -0500

    adding smtp send text file

---
+++ send_text_smptLib.py
@@ -0,0 +1,13 @@
+import smtplib
+
+def send_sms_via_email(phoneNum, message, carrier_gateway):
+    to = f"{phoneNum}@{carrier_gateway}"
+    from_email = "automation.email110@gmail.com"
+    password = "qokt lcqn ssnh rmxz"
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(from_email, password)
+        server.sendmail(from_email, to, message)
+
+send_sms_via_email('7135779932', 'Hello', 'tmomail.net')
